b6e940becfba646e074f0efc88ef1b8e57b2d5c5
Deepak Bhaskaran <deepak@duo.com>
2017-07-27 22:50:48 -0400

    Updated override.py with site name, tag line etc

--- indiangro/settings/override.py
+++ indiangro/settings/override.py
@@ -5,3 +5,6 @@ ALLOWED_HOSTS = ['63.142.254.51','localhost']
 PAYPAL_API_USERNAME = 'deepak.bhaskaran-facilitator_api1.gmail.com'
 PAYPAL_API_PASSWORD = 'TFX6V8DNNZ6ZTAY8'
 PAYPAL_API_SIGNATURE = 'AFcWxV21C7fd0v3bYYYRCpSSRl31AcHV4KiPTORgwMrl98rYKXeUCnfC'
+
+OSCAR_SHOP_NAME = 'IndianGrocery.us'
+OSCAR_SHOP_TAGLINE = 'Shopping, simplified.'
